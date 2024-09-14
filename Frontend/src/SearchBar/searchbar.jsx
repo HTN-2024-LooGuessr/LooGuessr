4668b895173b8125ee8e3131f6de69aad3c982fd
Saarujan-Sathees <sparkwolf2006@gmail.com>
2024-09-14 15:50:24 -0400

    Added Search Functionality

--- Frontend/src/SearchBar/searchbar.jsx
+++ Frontend/src/SearchBar/searchbar.jsx
@@ -1,10 +1,15 @@
 import './searchbar.css'
 
+function onSearch(ev = new InputEvent()) {
+    const search = document.getElementById('searchbar');
+    
+}
+
 export default function SearchBar() {
     return (
         <>
-            <input id='searchbar' autoCapitalize='none'></input>
+            <input id='searchbar' autoCapitalize='none' onKeyUp={onSearch}></input>
             <span id='searchbutton' className='material-symbols-rounded'>search</span>
         </>
-    )
+    );
 }
