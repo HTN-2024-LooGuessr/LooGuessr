f59fccc128a00872c8f914939a0803b6ae5aea85
jonathanzhu <jiasunzhu@gmail.com>
2024-09-14 19:04:59 -0400

    add: login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
-export default function Login() {
+
+export default function Login(props) {
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        const email = document.getElementById("email").value;
+        const pw = document.getElementById("pw").value;
+        console.log(email + " " + pw)
+        e.preventDefault()
+        axios.post("http://localhost:5555/user/login", {
+            email: email,
+            password: pw
+        }).then((res) => {
+            console.log(res.data.verdict)
+            if (res.data.verdict) {
+                props.login(res.data._id)
+                navigate("/LooGuessr")
+            }else {
+                alert("Incorrect password or email")
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
     return (
+        props.uid === "" ?
         <div>
-
-        </div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor={"email"}>email:</label><br/>
+                <input type="text" id="email"/><br/>
+                <label htmlFor={"pw"}>password:</label><br/>
+                <input type="text" id="pw"/><br/>
+                <input type="submit"/>
+            </form>
+        </div> : navigate("/LooGuessr")
     );
+}
+
+Login.prototype = {
+    login: PropTypes.func
 }
