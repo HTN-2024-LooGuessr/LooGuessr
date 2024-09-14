8aad4676982f85aeda689a5b34686992c5b54189
Saarujan-Sathees <sparkwolf2006@gmail.com>
2024-09-14 13:54:58 -0400

    Testing NavBar Folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import NavBar from './navbar'
+import NavBar from './NavBar/navbar'
 
 export default function App() {
   const [count, setCount] = useState(0)
