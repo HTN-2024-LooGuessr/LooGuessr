3711ae19ac0b93a1f8def8bf0ac70175c3b8372d
Saarujan Sathees <85376227+Saarujan-Sathees@users.noreply.github.com>
2024-09-14 03:05:38 -0400

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,5 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-}
