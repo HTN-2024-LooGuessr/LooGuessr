d0eb327e54324718dec29d743f36714fb3043aab
jonathanzhu <jiasunzhu@gmail.com>
2024-09-14 03:34:29 -0400

    Added npm packages

--- Client/app.jsx
+++
@@ -1,4 +0,0 @@
-<head>
-    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-</head>
