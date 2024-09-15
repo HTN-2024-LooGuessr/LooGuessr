a2aff2dbab8e709fad55f012ad620473f8db0009
Saarujan-Sathees <sparkwolf2006@gmail.com>
2024-09-14 22:16:14 -0400

    Added Map & Image Display Foundations

--- Frontend/src/Friends/Friends.jsx
+++ Frontend/src/Friends/Friends.jsx
@@ -57,10 +57,104 @@ export default function FriendList(props) {
 }
 
 function Friend(props) {
+    function handleMapGuess() { //NAMAN
+
+    }
+
+    function createStory(imageURL, displayGuesses = false) {
+        const story = document.createElement("div");
+        story.className = "story";
+        story.style = `background-color: red; opacity: 0;`;
+
+        const backButton = document.createElement("button");
+        backButton.id = "backButton";
+        backButton.className = "storyButton";
+        backButton.addEventListener("click", async ev => {
+            story.style.opacity = "0";
+            story.style.pointerEvents = "none";
+            document.getElementById("navbar").style.opacity = 1;
+            await new Promise(r => setTimeout(r, 500));
+            if (story.parentElement == document.body) document.body.removeChild(story);
+        })
+
+        const backIcon = document.createElement("span");
+        backIcon.id = "backButtonIcon";
+        backIcon.className = "material-symbols-rounded";
+        backIcon.textContent = "arrow_back_ios";
+        backIcon.style = "width: 1.75vh; font-size: 3.5vh";
+
+        const switchButton = document.createElement("button");
+        switchButton.id = "storySwapButton";
+        switchButton.className = "storyButton";
+
+        const switchIcon = document.createElement("span");
+        switchIcon.className = "material-symbols-rounded";
+        switchIcon.id = "storySwapIcon";
+        switchIcon.style = "font-size: 4vh; font-variation-settings: 'FILL' 1, 'wght' 300, 'GRAD' 0, 'opsz' 24;";
+
+        const image = document.createElement('div');
+        image.className = "storyImage";
+        image.style = `background: url(${imageURL}); background-color: green`;
+
+        const map = document.createElement("div");
+        map.className = "mapContainer";
+        map.style = "";
+
+        if (displayGuesses) {
+            story.appendChild(map);
+            switchIcon.textContent = "image";
+            switchIcon.style.fontVariationSettings = "font-variation-settings: 'FILL' 0, 'wght' 300, 'GRAD' 0, 'opsz' 24;";
+        } else {
+            story.appendChild(image);
+            switchIcon.textContent = "person_pin_circle";
+        }
+
+        switchButton.addEventListener("click", ev => {
+            if (switchIcon.textContent == "person_pin_circle") {
+                story.removeChild(image);
+                story.appendChild(map);
+                switchIcon.textContent = "image";
+                switchIcon.style.fontVariationSettings = "font-variation-settings: 'FILL' 0, 'wght' 300, 'GRAD' 0, 'opsz' 24;";
+                handleMapGuess();
+            } else {
+                story.removeChild(map);
+                story.appendChild(image);
+                switchIcon.textContent = "person_pin_circle";
+                switchIcon.style.fontVariationSettings = "font-variation-settings: 'FILL' 1, 'wght' 300, 'GRAD' 0, 'opsz' 24;";
+            }
+        });
+
+        backButton.appendChild(backIcon);
+        switchButton.appendChild(switchIcon);
+        story.appendChild(backButton);
+        story.appendChild(switchButton);
+
+        return story;
+    }
+
+    async function click() {
+        const username = props.username;
+        const response = { image: "C:\\Users\\saaru\\Downloads\\Capture1726354644.jpg", status: "other" }//SENDTOSERVER(getUsernamePhoto);
+
+        switch (response.status) {
+            case "guessMade": //Display your guess and the guess of other users
+                break;
+            case "noPhotos": //User hasn't posted a photo yet (or their photo expired after 24 hours); Do Nothing
+                break;
+            default: //You hasn't made a guess / You haven't viewed their new image; show their image
+                const story = createStory(response.image, response.status == "guessMade");
+                document.body.appendChild(story);   
+                document.getElementById("navbar").style.opacity = 0;
+                await new Promise(r => setTimeout(r, 50));
+                story.style.opacity = "1";
+                break;
+        }
+    }
+
     return (
-        <button className="friend">
+        <button className="friend" onClick={click}>
             <img className="profileImg" src={props.imgSrc}></img>
-            <pre className="friendName">{props.username}</pre>
+            <pre className="friendName" id={`friend-${props.username}-id`}>{props.username}</pre>
         </button>
     )
 }
--- Frontend/src/NavBar/Navbar.jsx
+++ Frontend/src/NavBar/Navbar.jsx
@@ -1,13 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, createRef } from 'react'
 import './navbar.css'
 import { useNavigate } from 'react-router-dom'
 
 export default function NavBar() {
+    const page = window.location.pathname.includes("photo") ? "photo" : 
+                 window.location.pathname.includes("profile") ? "profile" : 
+                 window.location.pathname.endsWith("LooGuessr/") ? "home" :
+                 "other";
+
+    /*const photoRef = createRef(), profileRef = createRef(), homeRef = createRef();
+    useEffect(() => {
+        switch (page) {
+            case "photo": photoRef.current.focus(); break;
+            case "profile": profileRef.current.focus(); break;
+            case "home": homeRef.current.focus(); break;
+            case "other": 
+                photoRef.current.blur();
+                profileRef.current.blur();
+                homeRef.current.blur();
+        }
+    }, []);*/
+
     return (
         <div id="navbar">
             <NavButton icon="add_a_photo" id="photoTab" borderRadius="15px 0 0 15px" redirect="photo"></NavButton>
-            <NavButton icon="dashboard" redirect=""></NavButton>
-            <NavButton icon="bar_chart" borderRadius="0 15px 15px 0" redirect="profile"></NavButton>
+            <NavButton icon="dashboard" id="homeTab" redirect=""></NavButton>
+            <NavButton icon="bar_chart" id="profileTab" borderRadius="0 15px 15px 0" redirect="profile"></NavButton>
         </div>
     )
 }
