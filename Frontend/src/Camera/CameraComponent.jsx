e4ba137e6627b7e545ddacb688c7b2b6f9aa211e
shougangu <dashoug001@gmail.com>
2024-09-15 03:36:57 -0400

    Add Camera floor seelctor

--- Frontend/src/Camera/CameraComponent.jsx
+++ Frontend/src/Camera/CameraComponent.jsx
@@ -1,8 +1,9 @@
 import React, {useCallback}from 'react';
 import './CameraComponent.css';
 import axios from "axios";
-
+import { useState } from 'react';
 export default function CameraComponent() {
+    const [curFloorID, setCurFloorID] = useState("m_e6c96a31fba4ef51")
     async function capture() {
         const video = document.getElementById("cameraFeed") 
         const canvas = document.createElement("canvas");
@@ -37,8 +38,41 @@ export default function CameraComponent() {
 
     return (
         <>
+        <select
+        onChange={(e) => {
+          setCurFloorID(e.target.value);
+        }}
+        title="level select camera"
+        style={{ 
+            position: 'absolute', 
+            top: 10, 
+            left: 10, 
+            backgroundColor: 'rgb(170 100 180)', /* Purple background */
+            color: '#000000',
+            fontFamily: 'Arial',  
+            fontSize: 16, 
+            borderRadius: '15px', /* Rounded corners */
+            padding: '10px 20px', 
+            border: 'none', 
+            outline: 'none', 
+            cursor: 'pointer', 
+            transition: 'background-color 0.3s ease',
+            zIndex: 1000 /* Ensure the select is on top */
+        }}>
+
+        <option> Floor 1</option>
+        <option> Floor 3</option>
+        <option> Floor 2</option>
+        <option> Floor 4</option>
+        <option> Floor 6</option>
+        <option> Floor 5</option>
+        <option> Floor 7</option>
+
+        </select>
+        <>
             <video autoPlay playsInline id="cameraFeed"></video>
             <input id='shutter' onClick={capture} role='button' type='button' capture="camera"></input>
+        </>
         </>
     );
 };
